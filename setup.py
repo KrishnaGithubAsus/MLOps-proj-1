a9f1a70fdf161ab3ebf650917e18fefa0ee69e8d
Krishna Kumar <krishnaasus15@gmail.com>
2025-06-06 11:35:56 +0530

    Project structure and env setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Krishna Kumar",
+    author_email="krishnaasus15@gmail.com",
+    packages=find_packages()
+)
